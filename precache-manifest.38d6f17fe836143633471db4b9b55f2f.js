daa278eab63bc0f76619eb7ec20f6798cf664315
Azat <razatr@yandex.ru>
2021-04-21 10:46:49 +0300

    Updates

---
+++ precache-manifest.38d6f17fe836143633471db4b9b55f2f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9a2fd26bd90f022a7b7e69d4f7455221",
+    "url": "/Lab-7/index.html"
+  },
+  {
+    "revision": "9d484b948b167b3a23e4",
+    "url": "/Lab-7/static/js/2.dac3d163.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/Lab-7/static/js/2.dac3d163.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9cdfe6f4b1c06c8eded6",
+    "url": "/Lab-7/static/js/main.897a1410.chunk.js"
+  },
+  {
+    "revision": "15c246574e74da49ecde",
+    "url": "/Lab-7/static/js/runtime-main.c2f91131.js"
+  }
+]);
